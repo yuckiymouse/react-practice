e49f31a4f10960be47087fd23a42bb1ebced20b2
Yuki Ueda <yukiueda@yukinomac-mini.lan>
2022-01-03 18:38:43 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default App
